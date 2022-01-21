393b2678772fc27c7865602e9eac51ec998249ec
Radu Nemerenco <r.nemerenco@gmail.com>
2022-01-21 16:53:12 +0200

    update the index page description

--- pages/index.tsx
+++ pages/index.tsx
@@ -72,8 +72,8 @@ const Home: NextPage = () => {
           </div>
         )}
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
+          Choose one by clicking the cards below{' '}
+          {/*<code className={styles.code}>pages/index.tsx</code>*/}
         </p>
 
         <div className={styles.grid}>
